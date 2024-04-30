848e96d8860a22d46b5032897c04cdf63bd9e6fe
sangwook16074 <qazws78941@gmail.com>
2024-04-30 18:46:32 +0900

    number

---
+++ front/number.dart
@@ -0,0 +1,26 @@
+void main() {
+  // int age = 26;
+  // print(age);
+  // age = 30;
+  // print(age);
+
+  // double pi = 3.14;
+  // print(pi);
+
+  // int a = 10;
+  // int b = 2;
+
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a % b);
+
+  double a = 1.1;
+  double b = 0.5;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+}
