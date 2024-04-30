28bcb4562f6e7c33954d72d083b7990fa0ae9326
sangwook16074 <qazws78941@gmail.com>
2024-04-30 19:26:10 +0900

    oper

---
+++ front/oper.dart
@@ -0,0 +1,28 @@
+void main() {
+  int a = 6;
+  int b = 1;
+  // print(a >= b);
+  // print(a);
+  // a = a + 3;
+  // print(a);
+  // a = a + 1;
+  // print(a);
+  // a++;
+  // print(a);
+  // ++a;
+  // print(a);
+
+  // a = a + 3;
+  // a += 3;
+  // a = a + 1;
+  // a += 1;
+  // a++;
+  int c = 3;
+  // print(c++);
+  // print(c);
+  // print(++c);
+
+  print(c--);
+  print(c);
+  print(--c);
+}
