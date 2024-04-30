25106bbe8e4907475951d5c250dc94aec31197f5
sangwook16074 <qazws78941@gmail.com>
2024-04-30 19:52:48 +0900

    if else

---
+++ front/if.dart
@@ -0,0 +1,34 @@
+void main() {
+  // int a = 20;
+  // int b = 10;
+
+  // if (a > b)
+  //   print("a가 더 크다.");
+  // else if (a == b)
+  //   print("a랑 b가 같다.");
+  // else
+  //   print("b가 더 크다.");
+
+  // print(a + b);
+
+  int number = 10;
+  switch (number) {
+    case 5:
+      print("5입니다.");
+      break;
+    case 6:
+      print("6입니다.");
+      break;
+    case 7:
+      print("7입니다.");
+      break;
+    case 8:
+      print("8입니다.");
+      break;
+    case 9:
+      print("9입니다.");
+      break;
+    default:
+      print("값이 없습니다.");
+  }
+}
