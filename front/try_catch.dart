eca6c733b9a1e5228bcca71b49c644632089df7a
sangwook16074 <qazws78941@gmail.com>
2024-05-04 09:52:34 +0900

    try catch and assert

---
+++ front/try_catch.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 10;
+  int b = 0;
+  assert(b == 0, "나눠지는 숫자는 0이면 안된다.");
+  // var sum = a % b;
+
+  // try {
+  //   var sum = a  b;
+  //   print(sum);
+  // } catch (error) {
+  //   print(error);
+  //   var sum = 0;
+  // } finally {
+  //   print("로직 종료");
+  // }
+}
