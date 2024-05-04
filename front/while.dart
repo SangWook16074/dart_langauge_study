9606bedc595ef01471953a6cc49436edc78dc7b7
sangwook16074 <qazws78941@gmail.com>
2024-05-04 09:43:31 +0900

    while

---
+++ front/while.dart
@@ -0,0 +1,19 @@
+void main() {
+  int i = 0;
+
+  for (var i = 0; i < 10; i++) {
+    print(i);
+  }
+
+  int number = 3;
+
+  while (number == 3) {
+    print(i);
+    i++;
+  }
+
+  do {
+    print(i);
+    i--;
+  } while (i > 0);
+}
